311a22df4765626a4223ad98bae6b61f2bf7a054
kennycyb@gmail.com <kennycyb@gmail.com@2e3a63db-16a7-c70d-d1ed-c2b6f52095f3>
2010-06-28 08:13:43 +0000

    Refactoring

---
+++ trunk/java-ui-factory-core/src/main/java/com/wpl/ui/factory/impl/components/swing/JFrameFactory.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2010 Kenny Chong (wongpeiling.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wpl.ui.factory.impl.components.swing;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JFrame;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.wpl.ui.annotations.UiLayout;
+import com.wpl.ui.annotations.components.JFrameProperties;
+import com.wpl.ui.annotations.frame.UiFrameCloseOperation;
+import com.wpl.ui.enums.FrameCloseOperation;
+import com.wpl.ui.factory.ComponentContext;
+import com.wpl.ui.factory.impl.UiAnnotationHandler;
+import com.wpl.ui.factory.impl.components.awt.FrameFactory;
+import com.wpl.ui.layout.managers.NullLayout;
+
+/**
+ * 
+ * @author kenny
+ * @since 1.0
+ */
+public class JFrameFactory extends FrameFactory {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(JFrameFactory.class);
+
+	protected void frameCloseOperation(ComponentContext context,
+			JFrame component, FrameCloseOperation value) {
+		component.setDefaultCloseOperation(value.getSwingConstant());
+
+		LOGGER.debug("{}|JFrame.setDefaultCloseOperation({})", context.getId(),
+				value);
+	}
+
+	@UiAnnotationHandler(UiFrameCloseOperation.class)
+	protected void handleUiFrameCloseOperation(ComponentContext context,
+			JFrame component, UiFrameCloseOperation annotate) {
+		frameCloseOperation(context, component, annotate.value());
+	}
+
+	@UiAnnotationHandler(JFrameProperties.class)
+	protected void handleJFrameProperties(ComponentContext context,
+			JFrame component, JFrameProperties annotate) {
+
+		windowPosition(context, component, annotate.windowPosition());
+
+		if (annotate.height() > 0 && annotate.width() > 0) {
+			component.setSize(annotate.width(), annotate.height());
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("{}|JFrame.setSize({},{})", new Object[] {
+						context.getId(), annotate.width(), annotate.height() });
+			}
+			context.setPack(false);
+		}
+
+		frameCloseOperation(context, component, annotate.frameCloseOperation());
+
+		title(context, component, annotate.title());
+	}
+
+	@UiAnnotationHandler(UiLayout.class)
+	protected void handleUiLayout(ComponentContext context, JFrame component,
+			UiLayout annotate) {
+		if (annotate.value() == NullLayout.class) {
+			component.setLayout(null);
+			return;
+		}
+
+		try {
+
+			LayoutManager lm = annotate.value().newInstance();
+			component.setLayout(lm);
+
+			LOGGER.debug("{}|using layout {}", context.getId(), annotate
+					.value());
+
+		} catch (InstantiationException e) {
+			LOGGER.error("UiLayout - InstantiationException - {} - {} ",
+					annotate.value(), e.getMessage());
+		} catch (IllegalAccessException e) {
+			LOGGER.error("UiLayout - IllegalAccessException - {} - {} ",
+					annotate.value(), e.getMessage());
+		}
+	}
+}
---
+++ trunk/java-ui-factory-core/src/main/java/com/wpl/ui/factory/impl/components/swing/JTextFieldFactory.java
@@ -0,0 +1,60 @@
+/*
+/*
+ * Copyright 2010 Kenny Chong (wongpeiling.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wpl.ui.factory.impl.components.swing;
+
+import javax.swing.JTextField;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.wpl.ui.annotations.UiColumns;
+import com.wpl.ui.annotations.components.JTextFieldProperties;
+import com.wpl.ui.factory.ComponentContext;
+import com.wpl.ui.factory.impl.UiAnnotationHandler;
+
+/**
+ * Factory that create JTextField
+ * 
+ * @author kenny
+ * @since 1.0
+ */
+public class JTextFieldFactory extends JTextComponentFactory {
+
+	private static Logger LOGGER = LoggerFactory
+			.getLogger(JTextFieldFactory.class);
+
+	@UiAnnotationHandler(UiColumns.class)
+	void handleAnnotation(ComponentContext context, JTextField component,
+			UiColumns annotate) {
+
+		component.setColumns(annotate.value() < 0 ? 0 : annotate.value());
+
+		LOGGER.debug("{}|JTextField.setColumns({})", context.getId(), annotate
+				.value());
+	}
+
+	@UiAnnotationHandler(JTextFieldProperties.class)
+	void handleAnnotation(ComponentContext context, JTextField component,
+			JTextFieldProperties annotate) {
+
+		component.setText(annotate.text());
+
+		LOGGER.debug("{}|JTextField.setText(\"{}\")", context.getId(), annotate
+				.text());
+	}
+
+}
